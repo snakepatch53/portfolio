4f23c04671dd61042503574eb72a8c8c43956eab
snakepatch53 <snakepatch53@gmail.com>
2023-01-22 17:31:57 -0500

    updated

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -11,14 +12,17 @@ $dotenv->load();
 #region HTTPS Redirection
 $_ACTUAL_DOMAIN = $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
 $_IS_PRODUCTION = (str_contains($_ACTUAL_DOMAIN, $_ENV['HTTP_PATH_PRODUCTION'])) ? true : false;
-$_IS_HTTPS = (isset($_SERVER['HTTPS']) && $_SERVER['https'] === 'on') ? 'true' : 'false';
+$_IS_HTTPS = (isset($_SERVER['HTTPS'])) ? 'true' : 'false';
 if (!$_IS_PRODUCTION) $_ENV['HTTP_PATH'] = 'http://' . $_ENV['HTTP_PATH_LOCAL'];
-else $_ENV['HTTP_PATH'] = (($_ENV['PROTOCOL_HTTPS_PRODUCTION'] == 'true') ? 'https' : 'http') . '://' . $_ENV['STATIC_PATH_PRODUCTION'];
+else $_ENV['HTTP_PATH'] = (($_ENV['PROTOCOL_HTTPS_PRODUCTION'] == 'true') ? 'https' : 'http') . '://' . $_ENV['HTTP_PATH_PRODUCTION'];
 $_ENV['STATIC_PATH'] = $_ENV['HTTP_PATH'] . $_ENV['STATIC_PATH'];
-if ($_IS_PRODUCTION && $_IS_HTTPS != $_ENV['HTTPS']) {
-    header('locaton: ' . $_ENV['HTTP_PATH']);
+if ($_IS_PRODUCTION && $_IS_HTTPS != $_ENV['PROTOCOL_HTTPS_PRODUCTION']) {
+    // echo "xd";
+
+    header('location: ' . $_ENV['HTTP_PATH']);
     exit();
 }
+// echo $_ENV['HTTP_PATH'];
 #endregion
 
 $router = new \Bramus\Router\Router();
